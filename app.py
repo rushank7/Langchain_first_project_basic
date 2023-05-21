b1e10c5c8105a682a986787d470b5b2e7a6e1d3c
rushank7 <rushank.2008@gmail.com>
2023-05-21 12:23:09 +0200

    simplesequentialchain with muliple outputs

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
-from langchain.chains import LLMChain, SimpleSequentialChain ## simplesequentialchain will help to genrate multple outputs
-from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SimpleSequentialChain , SequentialChain ## simplesequentialchain will help to genrate multple outputs
+from langchain.prompts import PromptTemplate  ## sequential chain will aloow us to get multiple outputs
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -15,15 +17,22 @@ prompt = st.text_input('Write your prompt over here')
 title_tempalte = PromptTemplate(input_variables=  ['topic'], template = 'write me a youtube video script on Title: {topic}')
 script_title_tempalte = PromptTemplate(input_variables=  ['topic'], template = 'write me a youtube video script based on this title  Title: {topic}')
 
+## memory
+memory = ConversationBufferMemory(input_key='topic',memory_key='chat_history')
+
 
 ## LLms
 
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm = llm, prompt = title_tempalte , verbose = True)
-script_chain = LLMChain(llm = llm, prompt = script_title_tempalte , verbose = True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain, verbose=True])
+title_chain = LLMChain(llm = llm, prompt = title_tempalte , verbose = True, output_key='title')
+script_chain = LLMChain(llm = llm, prompt = script_title_tempalte , verbose = True, output_key='script')
+sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], input_variables=['topic'],strip_variables=['title','script'] ,verbose=True)
 
 # show stuff to screen if there is a prompt
 if prompt:
-    response = sequential_chain.run(topic=prompt)
-    st.write(response)
+    response = sequential_chain.run({'topic':prompt})
+    st.write(response['title'])
+    st.write(response['script'])
+
+    with st.expander('Message history'):
+        st.info(memory.buffer)
